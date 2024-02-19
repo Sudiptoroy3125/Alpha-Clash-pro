c27a53f61fb63ff3b99026ffe66d014072fd5a6e
Sudipto Roy <freelancersudipto571@gmail.com>
2024-02-19 23:34:12 +0600

    life and score js complete

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -11,6 +11,50 @@
 //     // console.log(playGroundSection.classList);
 //     playGroundSection.classList.remove('hidden');
 // }
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+    console.log('player preesed', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+
+    // check matched or not
+    if(playerPressed === expectedAlphabet){
+        console.log('You got a point');
+        // update score:
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+
+
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('You lost a life');
+        // step 1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        // step 2: reduce the life count
+        const newLife = currentLife - 1;
+
+        // step 3: display the updated life count
+        currentLifeElement.innerText = newLife;
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress);
 function continueGame(){
     // step 1: generate a  random alphabet
     const alphabet = getARandomAlphabet();
